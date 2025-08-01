45b6bed3d565b3d3e64d0109ce5f0d675a053365
Ming Yang <67586052+MoeYang@users.noreply.github.com>
2025-08-01 12:33:38 +0000

    add python

---
+++ index.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder="static", template_folder=".")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/health")
+def health():
+    return "ok"
+
+if __name__ == "__main__":
+    # 监听 9000 端口（阿里云 FC 自定义镜像要求）
+    app.run(host="0.0.0.0", port=9000)
