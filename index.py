4f730b729edfd98a1ffd1e234d22042d61b706a4
Ming Yang <67586052+MoeYang@users.noreply.github.com>
2025-08-05 08:19:41 +0000

    add

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 from flask import Flask, render_template, Response
 
 app = Flask(__name__, static_folder="static", template_folder=".")
@@ -7,6 +8,18 @@ app = Flask(__name__, static_folder="static", template_folder=".")
 def home():
     html= render_template("index.html")
     return Response(html, mimetype="text/html")
+
+@app.route("/<page>.html")
+def render_html(page):
+    """
+    仅匹配以 .html 结尾的页面
+    /about.html -> about.html
+    """
+    filename = f"{page}.html"
+    if os.path.exists(os.path.join(app.template_folder, filename)):
+        return Response(render_template(filename), mimetype="text/html")
+    else:
+        return "404 Not Found", 404
 
 @app.route("/health")
 def health():
