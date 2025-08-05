3e2888ae5abf11249f90ee0729923594b01b1ca5
Ming Yang <67586052+MoeYang@users.noreply.github.com>
2025-08-05 07:26:58 +0000

    update

--- index.py
+++ index.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 
 app = Flask(__name__, static_folder="static", template_folder=".")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    html= render_template("index.html")
+    return Response(html, mimetype="text/html")
 
 @app.route("/health")
 def health():
